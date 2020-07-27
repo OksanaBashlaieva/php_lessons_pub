86e9ef9c72f66b2bfa7575fd7e7f831d38a2f682
OksanaBashlaieva <68867292+OksanaBashlaieva@users.noreply.github.com>
2020-07-27 20:21:14 +0300

    Add files via upload

---
+++ php-lessons.sana/www/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+echo "Hello, NIX Education (echo) \r \n";
+
+$str = 'Hello, NIX Education - 1' ;
+echo $str;
+echo "\r \n";
+
+
+$str = "Hello, NIX Education - 2 \r \n" ;
+$str1 = $str;
+
+echo $str1;
+
+phpinfo();
+
+?>
